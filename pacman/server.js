c4358fe209bd5ca3dde9bd0816feb3e0d73489fa
teuno <teunohooijer@yahoo.com>
2017-03-30 13:54:00 +0200

    alles over server

--- pacman/server.js
+++ pacman/server.js
@@ -4,19 +4,30 @@ const http = require('http')
 const readline = require('readline')
 const timer = require('timers')
 
+const FPS = 1000 / 25
+const Direction = {
+    NEUTRAL: 0,
+    RIGHT: 39,
+    DOWN: 40,
+    LEFT: 37,
+    UP: 38
+}
+
 const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
+    input: process.stdin,
+    output: process.stdout
 })
 
 
 const hostname = '127.0.0.1'
 const port = 3000
 
+let players = {}
+
 // Sets the server and loads the webpage.
 const server = http.createServer((req, res) => {
-  res.statusCode = 201
-  res.end(fs.readFileSync('index.html'))
+    res.statusCode = 201
+    res.end(fs.readFileSync('index.html'))
 })
 
 const io = require('socket.io').listen(server)
@@ -24,27 +35,48 @@ const io = require('socket.io').listen(server)
 // Handles the websocket client connect.
 io.sockets.on("connection", socket => {
     console.log("User connected: " + socket.id)
-    socket.on("ClientMessage", (key, agent) => handleInput(key, agent))
-
-    socket.on("updatePlayerLocation", updatePlayerLocation)
-
-    function updatePlayerLocation(data) {
-        console.log(data)
-        socket.broadcast.emit("updatePlayerLocation", data)
+    players[socket.id] = {
+        coords: { x: 0, y: 0 },
+        direction: Direction.NEUTRAL
     }
+    socket.emit("connected", players)
+    socket.emit("connectedYou", socket.id)
+    
 
+    socket.on("disconnect", function () {
+        console.log("User disconnected: " + socket.id)
+        delete players[socket.id]
+        socket.emit("disconnected", players)
+    })
+
+    socket.on("updatePlayerDirection", function (data) {
+        console.log(data)
+        players[socket.id]["direction"] = data
+        players[socket.id]["coords"]["x"] += 10
+    })
+
+    socket.on("ClientMessage", (key, agent) => handleInput(key, agent))
 })
 
 
 
+
+
+setInterval(function () {
+    io.sockets.emit("updatePlayerPosition", players)
+
+}, FPS)
+
+
+
 const handleInput = (key, agent) => {
-  console.log(key)
-  io.emit('UpdatePlayer',{key, agent})
+    //console.log(key)
+    io.emit('UpdatePlayer', { key, agent })
 }
 
 // Sends a message to all clients.
-rl.on('line', input =>io.emit('ServerMessage', input))
+rl.on('line', input => io.emit('ServerMessage', input))
 
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`)
+    console.log(`Server running at http://${hostname}:${port}/`)
 })
