653dccf0861b3a13e375d56eb5abdbe13a674add
teuno <teunohooijer@yahoo.com>
2017-03-31 14:27:19 +0200

    Menu laad en gaat naar spel en testTabel

---
+++ load.js
@@ -0,0 +1,41 @@
+"use strict"
+const select = selector => document.querySelector(selector)
+const load = callback => {
+    select('div').remove()
+    let div = document.createElement('div')
+
+	let canvas = document.createElement('canvas')
+	canvas.setAttribute('width', '224')
+    canvas.setAttribute('height', '288')
+    div.appendChild(canvas)
+
+    let span = document.createElement('span')
+    span.innerHTML = '<button onclick="loadMenu()" ></button>'
+    div.appendChild(span)
+    select('body').appendChild(div)
+   
+
+	let script = document.createElement('script')
+	script.setAttribute("src",'client.js')
+	select("head").appendChild(script)
+}
+
+window.onkeydown = e => e.keyCode===32 && load()
+
+
+const loadMenu = callback => { 
+    select('div').remove()
+    let div = document.createElement('div')
+    div.innerHTML = document.getElementById('menuScript').innerHTML;
+    select('body').appendChild(div)
+}
+
+
+
+const loadTestTabel = callback => {
+    select('div').remove()
+    let div = document.createElement('div')
+    div.innerHTML = document.getElementById('testTabel').innerHTML;
+    select('body').appendChild(div)
+}
+
